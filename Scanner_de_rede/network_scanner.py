1aa7f2ffc1684d89d40a7dbbff75fea3155833db
Mariacmv <mariaclaragmviana@gmail.com>
2025-10-21 13:06:04 -0300

    Ajustando a saída dos resultados em dicionários

--- Scanner_de_rede/network_scanner.py
+++ Scanner_de_rede/network_scanner.py
@@ -3,8 +3,9 @@
 #Algoritmo escolhido no curso:
 #   1º: Criar uma requisição ARP direcionada a broadcast solicitando um IP (v)
 #   2º: Enviar o pacote e receber a resposta (v)
-#   3º: Parse the response - extrair informação de uma estrutura
-#   4º: Mostrar o resultado
+#   3º: Parse the response - extrair informação de uma estrutura (v)
+#   4º: Mostrar o resultado(v)
+#   5º: Criando uma lista de dicionários para melhorar a visualização da saída
 
 import scapy.all as scapy 
 
@@ -12,14 +13,23 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)  
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") #aqui o pacote está sendo enviado a um endereço broadcast, porém é possível direcionar com certeza para um dispositivo específico
     arp_request_broadcast = broadcast/arp_request 
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     #print(answered_list.summary())
     #print(unanswered_list.summary()) 
 
+    print("--------------------------------------------------------")
+    print("IP\t\t\tMAC ADDRESS") #'\t' é um tab - espaço
+    print("--------------------------------------------------------")
+
+    clients_list = [] #crio uma lista que vai armazenar os conjuntos de valores em um dicionário
+
     for item in answered_list:
-        print(item[1].psrc) #printa o endereço ip de quem respondeu
-        print(item[1].hwsrc) #printa o endereço mac de quem respondeu
-        print("----------------------------------------------------")
+        client_dict = {'ip':item[1].psrc, 'mac':item[1].hwsrc} #crio o dicionário determinando quais valores serão armazenados e como
+        clients_list.append(client_dict) #adiciono cada item do dicionário a lista 
+        print(item[1].psrc + "\t\t" + item[1].hwsrc)  #junto para printar na mesma linha
+    print(clients_list)
 
 scan("10.0.2.1/24")
 #scan("10.0.2.2")
+
+#para mostrar menos informações sobre o processo, utiliza-se a função 'verbose=False'
