d45fa442984e10925893fc1ee87857123f3ba0d8
Mariacmv <mariaclaragmviana@gmail.com>
2025-10-21 13:42:26 -0300

    FInalizando o scanner

--- Scanner_de_rede/network_scanner.py
+++ Scanner_de_rede/network_scanner.py
@@ -11,25 +11,24 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)  
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") #aqui o pacote está sendo enviado a um endereço broadcast, porém é possível direcionar com certeza para um dispositivo específico
-    arp_request_broadcast = broadcast/arp_request 
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") 
+    arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    #print(answered_list.summary())
-    #print(unanswered_list.summary()) 
 
-    print("--------------------------------------------------------")
-    print("IP\t\t\tMAC ADDRESS") #'\t' é um tab - espaço
-    print("--------------------------------------------------------")
-
-    clients_list = [] #crio uma lista que vai armazenar os conjuntos de valores em um dicionário
+    clients_list = []
 
     for item in answered_list:
-        client_dict = {'ip':item[1].psrc, 'mac':item[1].hwsrc} #crio o dicionário determinando quais valores serão armazenados e como
-        clients_list.append(client_dict) #adiciono cada item do dicionário a lista 
-        print(item[1].psrc + "\t\t" + item[1].hwsrc)  #junto para printar na mesma linha
-    print(clients_list)
+        client_dict = {'ip':item[1].psrc, 'mac':item[1].hwsrc} 
+        clients_list.append(client_dict)  
+    return clients_list
 
-scan("10.0.2.1/24")
-#scan("10.0.2.2")
+def print_result(results_list):
+    print("--------------------------------------------------------")
+    print("IP\t\t\tMAC ADDRESS")
+    print("--------------------------------------------------------")
 
-#para mostrar menos informações sobre o processo, utiliza-se a função 'verbose=False'
+    for client in results_list:
+        print(client['ip'] + '\t\t' + client['mac'])
+
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
